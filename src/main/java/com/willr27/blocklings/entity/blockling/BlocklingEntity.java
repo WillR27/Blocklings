00eb78ddcbe7dab508eeba5b2678952feb230ac7
Will <willr27398@gmail.com>
2022-06-19 09:21:25 +0100

    Add emerald passive ability

--- src/main/java/com/willr27/blocklings/entity/blockling/BlocklingEntity.java
+++ src/main/java/com/willr27/blocklings/entity/blockling/BlocklingEntity.java
@@ -509,6 +509,18 @@ public class BlocklingEntity extends TameableEntity implements IEntityAdditional
                    }
                }
            }
+
+           if (naturalBlocklingType == BlocklingType.EMERALD || blocklingType == BlocklingType.EMERALD)
+           {
+               LivingEntity owner = getOwner();
+               final float range = 8.0f;
+               final int level = 1;
+
+               if (owner != null && owner.distanceToSqr(this) < range * range)
+               {
+                    owner.addEffect(new EffectInstance(Effects.LUCK, 205, level - 1, false, false, true));
+               }
+           }
         }
 
         updateLightPos(false);
